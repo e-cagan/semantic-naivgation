399920fca372c17f361359f23266d9947254064c
e-cagan <emincaganapaydin@gmail.com>
2026-07-11 22:23:20 +0300

    feat: Initial repository structure pushed.

---
+++ src/semantic_nav_detector/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'semantic_nav_detector'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Emin Cagan Apaydin',
+    maintainer_email='emin@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'semantic_detector_node = semantic_nav_detector.semantic_detector_node:main'
+        ],
+    },
+)
